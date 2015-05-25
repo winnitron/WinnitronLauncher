f2e3b8159113cd9abdaa9cad5827bf324eaa2196
Marlon Wiebe <mr.m.wiebe@gmail.com>
2015-05-24 23:31:29 -0300

    Stabilized the background planes.

--- Assets/Scripts/View/BackgroundPlane.cs
+++ Assets/Scripts/View/BackgroundPlane.cs
@@ -2,23 +2,7 @@
 using System.Collections;
 
 public class BackgroundPlane : MonoBehaviour {
-    
-    public GameObject currentPlane;
-    public  GameObject otherPlane;
 
-    Vector3 startPos;                       // Position to move the plane too when its out of view
-
-    public float speed = 5;
-
-    float diff;
-    
-
-    void Start() {
-
-        diff = otherPlane.transform.position.z - currentPlane.transform.position.z;
-
-        startPos = currentPlane.transform.position;        
-    }
 
     public void scrollVertical() {
         
@@ -26,19 +10,4 @@ public class BackgroundPlane : MonoBehaviour {
         GetComponent<Animation>().Play();
     }
 
-	void Update () {
-
-        // Create a new plane when the current one has gone far enough
-        if (otherPlane.transform.localPosition.z > 470) {
-
-            otherPlane.transform.position = new Vector3(otherPlane.transform.position.x, otherPlane.transform.position.y, otherPlane.transform.position.z - (diff * 2));
-
-            var temp = otherPlane;
-            otherPlane = currentPlane;
-            currentPlane = temp;
-        }
-
-        currentPlane.GetComponent<Rigidbody>().velocity = Vector3.forward * speed;
-        otherPlane.GetComponent<Rigidbody>().velocity = Vector3.forward * speed;     
-	}     
 }
---
+++ Assets/Scripts/View/TesselatedPlane.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TesselatedPlane : MonoBehaviour {
+
+	public Transform resetPoint;
+
+	public float endPoint;
+
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.z > endPoint) {
+			gameObject.transform.position = resetPoint.position;
+		}
+	}
+}
