6b73242c4198801109938bc01167e65aaa296316
Marlon Wiebe <mr.m.wiebe@gmail.com>
2016-10-17 09:43:55 -0400

    Launcher now lets you know if the Options folder is missing.

--- Assets/Scripts/System/OptionsManager.cs
+++ Assets/Scripts/System/OptionsManager.cs
@@ -12,7 +12,7 @@ public class OptionsManager : MonoBehaviour {
     public bool initializing = true;
 
     //Default Folders
-    public string contentPath = "c:/WINNITRON/Winnitron_Data";
+    public string contentPath = "C:/WINNITRON/WINNITRON_Data";
     public string playlistsPath = "/Playlists";
     public string musicPath = "/Music";
     public string attractPath = "/Attract";
@@ -22,69 +22,71 @@ public class OptionsManager : MonoBehaviour {
 	//language
 	public JSONNode language;
 
+    //Options JSON
+    private JSONNode O;
+
 	// Use this for initialization
 	void Awake () {
 
-        //Load that JSON
-        var O = GM.data.LoadJson(Application.dataPath + "/Options/winnitron_options.json");
+        contentPath = Application.dataPath;
 
-        //Widescreen
-       if (O["launcher"]["widescreen"] == "false") widescreen = false;
+        //Check options folder!
+        if (!System.IO.Directory.Exists(contentPath + "/Options"))
+        {
+            initializing = false;
+            GM.Oops("No Options folder!  You may need to redownload from GitHub.");
+        }
 
-       //Force Mode
-       //If local only is checked, the launcher will not run the sync program at all and preserve all folders
-       //If remote only is checked, the launcher will nuke all folders and use the updated version only
-        if (O["launcher"]["forceMode"] == "localOnly") forceMode = ForceMode.LOCALONLY;
-        else if (O["launcher"]["forceMode"] == "remoteOnly") forceMode = ForceMode.REMOTEONLY;
-        else forceMode = ForceMode.NONE;
+        else
+        {
+            //Load that JSON
+            O = GM.data.LoadJson(Application.dataPath + "/Options/winnitron_options.json");
 
-        //Default Folders
-        string path = O["defaultFolders"]["root"];
-        if (path == "normal") contentPath = Application.dataPath;
-        else contentPath = O["defaultFolders"]["root"];
+            //Load language file
+            language = GM.data.LoadJson(contentPath + "/Options/winnitron_text_" + O["launcher"]["language"] + ".json");
 
-        GM.dbug.Log(this, "OPTIONS: Root path for content is " + contentPath);
 
-        //Find playlists path
-        //Checks for a ":" to see if it's a absolute or relative path
-        path = O["defaultFolders"]["playlists"];
-        if (path.Contains(":")) playlistsPath = path;   //absolute
-        playlistsPath = contentPath + path;             //relative
+            //Widescreen
+            if (O["launcher"]["widescreen"] == "false") widescreen = false;
 
-        GM.dbug.Log(this, "OPTIONS: Playlists path is: " + playlistsPath);
+            //Force Mode
+            //If local only is checked, the launcher will not run the sync program at all and preserve all folders
+            //If remote only is checked, the launcher will nuke all folders and use the updated version only
+            if (O["launcher"]["forceMode"] == "localOnly") forceMode = ForceMode.LOCALONLY;
+            else if (O["launcher"]["forceMode"] == "remoteOnly") forceMode = ForceMode.REMOTEONLY;
+            else forceMode = ForceMode.NONE;
 
-        //Find music path
-        path = O["defaultFolders"]["music"];
-        if (path.Contains(":")) musicPath = path;
-        else musicPath = contentPath + path;
+            //Default Folders
+            string path = O["defaultFolders"]["root"];
+            if (path == "normal") contentPath = Application.dataPath;
+            else contentPath = O["defaultFolders"]["root"];
 
-        GM.dbug.Log(this, "OPTIONS: Music path is " + musicPath);
+            GM.dbug.Log(this, "OPTIONS: Root path for content is " + contentPath);
 
-        //Find attract path
-        path = O["defaultFolders"]["attract"];
-        if (path.Contains(":")) attractPath = path;
-        else attractPath = contentPath + O["defaultFolders"]["attract"];
+            //Find playlists path
+            //Checks for a ":" to see if it's a absolute or relative path
+            path = O["defaultFolders"]["playlists"];
+            if (path.Contains(":")) playlistsPath = path;   //absolute
+            playlistsPath = contentPath + path;             //relative
 
-        GM.dbug.Log(this, "OPTIONS: Attract path is " + attractPath);
+            GM.dbug.Log(this, "OPTIONS: Playlists path is: " + playlistsPath);
 
-        //Find Sync Exe Path
-        path = O["defaultFolders"]["sync"];
-        if (path.Contains(":")) syncPath = path;
-        else syncPath = contentPath + path;
+            //Find music path
+            path = O["defaultFolders"]["music"];
+            if (path.Contains(":")) musicPath = path;
+            else musicPath = contentPath + path;
 
-        GM.dbug.Log(this, "OPTIONS: Sync path is " + syncPath);
+            GM.dbug.Log(this, "OPTIONS: Music path is " + musicPath);
 
-        //Find Sync Exe Path
-        path = O["defaultFolders"]["syncExe"];
-        if (path.Contains(":")) syncExe = path;
-        else syncExe = syncPath + path;
+            //Find attract path
+            path = O["defaultFolders"]["attract"];
+            if (path.Contains(":")) attractPath = path;
+            else attractPath = contentPath + O["defaultFolders"]["attract"];
 
-        GM.dbug.Log(this, "OPTIONS: Sync exe is " + syncExe);
+            GM.dbug.Log(this, "OPTIONS: Attract path is " + attractPath);
 
-        //Load language file
-        language = GM.data.LoadJson(contentPath + "/Options/winnitron_text_" + O["launcher"]["language"] + ".json");
-
-        initializing = false;
+            initializing = false;
+        }
     }
 
 	public string GetText(string category, string text)
