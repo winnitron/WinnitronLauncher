95c46476119d42032c86784e5d376989e178ceeb
Aaron Klaassen <aaron@outerspacehero.com>
2016-10-18 20:31:55 -0500

    Sync: delete games no longer in the subscribed playlist.

--- Assets/Scripts/System/GameSync.cs
+++ Assets/Scripts/System/GameSync.cs
@@ -22,29 +22,7 @@ public class GameSync : MonoBehaviour {
         Debug.Log("----------------------------- Running sync -----------------------------");
 
         initConfig();
-
         fetchPlaylistSubscriptions();
-
-        /*
-         * delete local playlists that aren't in the subscription list
-         * 
-         * for each playlist
-         * to_install = playlist.games
-         * get list of local (already installed) games  
-         * for each local
-         *   if not in playlist
-         *     delete
-         *   else
-         *     check metadata.json: if existing_install.installed_at >= game.last_modified
-         *     remove from to_install
-         * 
-         * for each to_install
-         *   mkdir (if it's not already there)
-         *   download
-         *   extract
-         *   write metadata
-         */
-
     }
 
     private void fetchPlaylistSubscriptions() {
@@ -61,59 +39,65 @@ public class GameSync : MonoBehaviour {
             Debug.Log("fetched playlists: " + www.text);
             var data = JSON.Parse(www.text);
             foreach (JSONNode playlist_data in data["playlists"].AsArray) {
-                playlists.Add(new Playlist(playlist_data));
+                playlists.Add(new Playlist(playlist_data, games_dir));
             }
 
+            // delete unsubscribed playlists
+            SluggedItem.deleteExtraDirectories(games_dir, playlists);
 
-            deleteUnsubscribedPlaylists();
-
+            foreach(Playlist playlist in playlists) {
+                playlist.syncGames();
+            }
 
         } else {
             Debug.Log("Error fetching playlists: " + www.error);
         }
     }
-
-    private void deleteUnsubscribedPlaylists() {
-        string[] installed = Directory.GetDirectories(games_dir);
-
-        foreach (string dir_full_path in installed) {
-            string directory = new DirectoryInfo(dir_full_path).Name;
-            if (directoryIsDeletable(directory, playlists)) {
-                Debug.Log("deleting " + directory);
-                Directory.Delete(dir_full_path, true);
-            }
-        }
-    }
         
-    // Careful here that you don't pass in a full path as `directory`
-    private bool directoryIsDeletable(string directory, ArrayList keepers) {
-        // Playlist or Game directories that start with an underscore are local additions,
-        // and won't be deleted just because they're not in the website data.
-        // In the future it'd be better to have that be a setting in the options or metadata json or something.
-        if (directory[0] == '_')
-            return false;
-
-        foreach (SluggedItem keeper in keepers) {
-            if (directory == keeper.slug)
-                return false;
-        }
-
-        return true;
-    }
-
-
 
     private class SluggedItem {
         public string title;
         public string slug;
+
+
+        public static void deleteExtraDirectories(string parent, ArrayList keepers) {
+            string[] installed = Directory.GetDirectories(parent);
+
+            foreach (string dir_full_path in installed) {
+                string directory = new DirectoryInfo(dir_full_path).Name;
+
+                if (SluggedItem.directoryIsDeletable(directory, keepers)) {
+                    Debug.Log("deleting " + dir_full_path);
+                    Directory.Delete(dir_full_path, true);
+                }
+            }
+        }
+
+        // Careful here that you don't pass in a full path as `directory`
+        private static bool directoryIsDeletable(string directory, ArrayList keepers) {
+            // Playlist or Game directories that start with an underscore are local additions,
+            // and won't be deleted just because they're not in the website data.
+            // In the future it'd be better to have that be a setting in the options or metadata json or something.
+            if (directory[0] == '_')
+                return false;
+
+            foreach (SluggedItem keeper in keepers) {
+                if (directory == keeper.slug)
+                    return false;
+            }
+
+            return true;
+        }
     }
 
     private class Playlist : SluggedItem {
         public ArrayList games = new ArrayList();
+        private string parentDirectory;
 
-        public Playlist(JSONNode data) {
+        public Playlist(JSONNode data, string parentDir) {
             title = data["title"];
             slug = data["slug"];
+            parentDirectory = parentDir;
 
             foreach(JSONNode game_data in data["games"].AsArray) {
                 games.Add(new Game(game_data));
@@ -121,14 +105,27 @@ public class GameSync : MonoBehaviour {
             
         }
 
-        public override string ToString() {
-            return "'" + title + "' (" + slug + ") " + games.Count + " games";
+        public void syncGames() {
+            Debug.Log("Syncing games for playlist '" + title + "'");
+            deleteRemovedGames();
+            /* TODO
+             * build to_install (new or updated games)
+             * download to_install
+             * extract
+             * write metadata
+             */
         }
+
+
+
+        private void deleteRemovedGames() {
+            SluggedItem.deleteExtraDirectories(parentDirectory + "/" + slug, games);
+        }
+
+
     }
 
     private class Game : SluggedItem {
-        public string title;
-        public string slug;
         public string description;
         public string downloadURL;
         public System.DateTime lastModified;
