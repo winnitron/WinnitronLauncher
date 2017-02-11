360289ef19279d0125418bacaadcb8201f3a492a
Marlon Wiebe <mr.m.wiebe@gmail.com>
2017-02-11 02:03:45 -0500

    Initial Progress on Pico8 Support
    Partway in getting Pico8 support working which is issue #45 .  Games
    launch, but do not quit properly.  Something with the AHK script.

--- Assets/Scripts/System/Runner.cs
+++ Assets/Scripts/System/Runner.cs
@@ -10,7 +10,12 @@ public class Runner : MonoBehaviour {
     Jukebox jukebox;
     Process gameRunner;
 
-    TextAsset tmp;
+    string templatesFolder = "AHK_templates/";
+
+    TextAsset ExeAHKTemplate;
+    TextAsset Pico8AHKTemplate;
+    TextAsset Pico8JS;
+
     TextAsset legacy; 
 
     void Awake()
@@ -26,8 +31,10 @@ public class Runner : MonoBehaviour {
         if(gameRunner == null)
             GM.Oops(GM.Text("error", "noLegacyControlExe"));
 
-        tmp = Resources.Load("RunGameTemplate") as TextAsset;
-        legacy = Resources.Load("WinnitronLegacy") as TextAsset;
+        ExeAHKTemplate = Resources.Load(templatesFolder + "ExeGameTemplate") as TextAsset;
+        Pico8AHKTemplate = Resources.Load(templatesFolder + "Pico8GameTemplate") as TextAsset;
+        Pico8JS = Resources.Load("Pico8Launcher") as TextAsset;
+        legacy = Resources.Load(templatesFolder + "WinnitronLegacy") as TextAsset;
     }
 
 
@@ -35,18 +42,10 @@ public class Runner : MonoBehaviour {
 
 	public void Run(Game game) {
 
-        if (System.IO.File.Exists(game.executable))
-        {
-            GM.dbug.Log(this, "Running Game " + game.name + " legacy: " + game.useLegacyControls);
+        GM.dbug.Log(this, "Running Game " + game.name + " legacy: " + game.useLegacyControls);
 
-            CreateAHKScript(game);
-            StartCoroutine(RunProcess(gameRunner));
-        }
-
-        else
-        {
-            GM.Oops(GM.Text("error", "couldNotFindGame"));
-        }
+        CreateAHKScript(game);
+        StartCoroutine(RunProcess(gameRunner));
 	}
 
 	IEnumerator RunProcess(Process process){
@@ -70,22 +69,46 @@ public class Runner : MonoBehaviour {
 
     private void CreateAHKScript(Game game)
     {
-        GM.dbug.Log(this, "RUNNER: Creat script game " + game.executable);
+        GM.dbug.Log(this, "RUNNER: Create script game " + game.executable);
+
+        string newAHKfile = "";
+
+        switch (game.gameType)
+        {
+            case Game.GameType.EXE:
+                newAHKfile = ExeAHKTemplate.text.Replace("{GAME_PATH}", game.executable);
+                
+                //Replace Keymaps
+                if (game.useLegacyControls) newAHKfile = newAHKfile.Replace("{KEYMAPS}", legacy.text);
+                else newAHKfile = newAHKfile.Replace("{KEYMAPS}", "");
+
+                break;
+
+            case Game.GameType.PICO8:
+                CreatePico8LauncherJS(game);
+                newAHKfile = Pico8AHKTemplate.text.Replace("{GAME_PATH}", GM.options.dataPath + "/Options/Pico8/nw.exe");
+                break;
+        }
 
         //Replace variables
-        string newString = tmp.text.Replace("{GAME_PATH}", game.executable);
-        newString = newString.Replace("{IDLE_TIME}", "" + GM.options.runnerSecondsIdle);
-        newString = newString.Replace("{IDLE_INITIAL}", "" + GM.options.runnerSecondsIdleInitial);
-        newString = newString.Replace("{ESC_HOLD}", "" + GM.options.runnerSecondsESCHeld);
-        
-
-        //Replace Keymaps
-        if (game.useLegacyControls) newString = newString.Replace("{KEYMAPS}", legacy.text);
-        else newString = newString.Replace("{KEYMAPS}", "");
+        newAHKfile = newAHKfile.Replace("{IDLE_TIME}", "" + GM.options.runnerSecondsIdle);
+        newAHKfile = newAHKfile.Replace("{IDLE_INITIAL}", "" + GM.options.runnerSecondsIdleInitial);
+        newAHKfile = newAHKfile.Replace("{ESC_HOLD}", "" + GM.options.runnerSecondsESCHeld);
 
         //Delete old file and write to new one
         File.Delete(Application.dataPath + "/Options/RunGame.ahk");
-        System.IO.File.WriteAllText(Application.dataPath + "/Options/RunGame.ahk", newString);
+        System.IO.File.WriteAllText(Application.dataPath + "/Options/RunGame.ahk", newAHKfile);
+    }
+
+    private void CreatePico8LauncherJS(Game game)
+    {
+        string newJS = "";
+
+        newJS = Pico8JS.text.Replace("{{{PATH_TO_HTML}}}", game.executable.Replace("\\", "\\\\"));
+
+        //Delete old file and write to new one
+        File.Delete(GM.options.dataPath + "/Options/Pico8/Pico8Launcher.js");
+        System.IO.File.WriteAllText(GM.options.dataPath + "/Options/Pico8/Pico8Launcher.js", newJS);
     }
 }
 
