c0ac677c78edb04ff4c08b5fb84ba55a4fe92e60
Aaron Klaassen <aaron@outerspacehero.com>
2017-09-04 15:29:59 -0500

    Hide ArgumentOutOfRangeException when it's just an empty Attract folder.

--- Assets/Scripts/System/AttractImgManager.cs
+++ Assets/Scripts/System/AttractImgManager.cs
@@ -8,90 +8,100 @@ using System.Linq;
 
 public class AttractImgManager :  MonoBehaviour {
 
-	public List<GameObject> attractSprites;
-	public GameObject attractSpritePrefab;
+    public List<GameObject> attractSprites;
+    public GameObject attractSpritePrefab;
 
-	public float timePassed = 0;
-	public int slideNum = 0;
+    public float timePassed = 0;
+    public int slideNum = 0;
 
-	public bool switchDelay = false;
+    public bool switchDelay = false;
 
-	void Start() {
-		BuildAttractImagesList();
-		ResetSlides();
-	}
+    void Start() {
+        BuildAttractImagesList();
+        ResetSlides();
+    }
 
-	void Update() {
+    void Update() {
 
         //Make sure you're only doing stuff if it's actually the Attract state
         //Cause pesky Unity might be running this even if this component is disabled
         //CAN'T TRUST IT
-		if (GM.state.worldState == StateManager.WorldState.Attract) {
+        if (GM.state.worldState == StateManager.WorldState.Attract) {
 
             //Go to launcher on any key pressed
             if (Input.anyKeyDown)
                 GM.state.ChangeState(StateManager.WorldState.Launcher);
 
             if (timePassed == 0) {
-				GM.logger.Debug(this, "hey!  first time running attract");
-				slideNum = -1;
-				NextSlide();
-			}
+                GM.logger.Debug(this, "hey!  first time running attract");
+                slideNum = -1;
+                NextSlide();
+            }
 
-			timePassed += Time.deltaTime;
+            timePassed += Time.deltaTime;
 
-			if(!attractSprites[slideNum].GetComponent<Animation>().isPlaying) NextSlide ();
+            try {
+                if(!attractSprites[slideNum].GetComponent<Animation>().isPlaying)
+                    NextSlide ();
+            } catch (ArgumentOutOfRangeException) {
+                // NOP. This gets thrown when the Attract folder is empty.
+            }
 
-		} else {
-			//start from the beginning next time
-			timePassed = 0;
-			ResetSlides();
-		}
-	}
+        } else {
+            //start from the beginning next time
+            timePassed = 0;
+            ResetSlides();
+        }
+    }
 
-	void ResetSlides() {
-		foreach (var asp in attractSprites) {
-			asp.GetComponent<Animation>().Rewind();
-			asp.GetComponent<Animation>().Stop();
-			asp.SetActive(false);
-		}
-	}
+    void ResetSlides() {
+        foreach (var asp in attractSprites) {
+            asp.GetComponent<Animation>().Rewind();
+            asp.GetComponent<Animation>().Stop();
+            asp.SetActive(false);
+        }
+    }
 
-	void NextSlide() {
-		if(slideNum >= 0) attractSprites [slideNum].SetActive (false);
+    void NextSlide() {
+        try {
+            if(slideNum >= 0) attractSprites [slideNum].SetActive (false);
 
-		slideNum += 1;
+            slideNum += 1;
 
-		GM.logger.Debug(this, "Calling Next Slide: " + slideNum + " of " + attractSprites.Count);
+            GM.logger.Debug(this, "Calling Next Slide: " + slideNum + " of " + attractSprites.Count);
 
-		if (slideNum > attractSprites.Count - 1) slideNum = 0;
+            if (slideNum > attractSprites.Count - 1) slideNum = 0;
 
-		attractSprites[slideNum].GetComponent<Animation>().enabled = true;
-		attractSprites[slideNum].GetComponent<Animation>().Rewind();
-		attractSprites[slideNum].SetActive(true);
-		attractSprites[slideNum].GetComponent<Animation>().Play();
-	}
+            attractSprites[slideNum].GetComponent<Animation>().enabled = true;
+            attractSprites[slideNum].GetComponent<Animation>().Rewind();
+            attractSprites[slideNum].SetActive(true);
+            attractSprites[slideNum].GetComponent<Animation>().Play();
+        } catch (ArgumentOutOfRangeException) {
+            // NOP. This gets thrown when the Attract folder is empty.
+        }
+    }
 
-	void BuildAttractImagesList() {
-    GM.logger.Info(this, "Attract path: " + GM.options.attractPath);
-		var attractDir = new DirectoryInfo(GM.options.attractPath);
+    void BuildAttractImagesList() {
+        GM.logger.Info(this, "Attract path: " + GM.options.attractPath);
+        var attractDir = new DirectoryInfo(GM.options.attractPath);
 
-		foreach (var attractImg in attractDir.GetFiles()) {
-			//Make sure only .png's get through!
-			GM.logger.Debug(this, "Loading image with ending: " + attractImg.FullName.Substring(Math.Max(0, attractImg.FullName.Length - 4)) + ".png");
-			if(attractImg.FullName.Substring(Math.Max(0, attractImg.FullName.Length - 4)) == ".png") {
-				GameObject newImgObj = Instantiate(attractSpritePrefab, new Vector3(0, -36.52403f, 0), transform.rotation) as GameObject;
+        foreach (var attractImg in attractDir.GetFiles()) {
+            //Make sure only .png's get through!
+            GM.logger.Debug(this, "Loading image with ending: " + attractImg.FullName.Substring(Math.Max(0, attractImg.FullName.Length - 4)) + ".png");
 
-				GM.logger.Debug(this, "Load Attract Image: " + attractImg);
-        // Load the screenshot from the games directory as a Texture2D
-        var SpriteTexture = new Texture2D(1024, 768);
-        SpriteTexture.LoadImage(File.ReadAllBytes(attractImg.FullName));
+            if(attractImg.FullName.Substring(Math.Max(0, attractImg.FullName.Length - 4)) == ".png") {
+                GameObject newImgObj = Instantiate(attractSpritePrefab, new Vector3(0, -36.52403f, 0), transform.rotation) as GameObject;
 
-				newImgObj.GetComponent<Image>().sprite = Sprite.Create(SpriteTexture, new Rect(0, 0, 1024, 768), new Vector2(0.5f, 0.5f));
-				newImgObj.transform.SetParent (transform);
+                GM.logger.Debug(this, "Load Attract Image: " + attractImg);
+                // Load the screenshot from the games directory as a Texture2D
+                var SpriteTexture = new Texture2D(1024, 768);
+                SpriteTexture.LoadImage(File.ReadAllBytes(attractImg.FullName));
 
-				attractSprites.Add(newImgObj);
-			}
-		}
-	}
+                newImgObj.GetComponent<Image>().sprite = Sprite.Create(SpriteTexture, new Rect(0, 0, 1024, 768), new Vector2(0.5f, 0.5f));
+                newImgObj.transform.SetParent (transform);
+
+                attractSprites.Add(newImgObj);
+            }
+        }
+    }
 }
