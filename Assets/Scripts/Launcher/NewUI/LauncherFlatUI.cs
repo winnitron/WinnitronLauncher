4c220a0067a78fbd7cbf7fa371fe81e7142e36b5
Marlon Wiebe <mr.m.wiebe@gmail.com>
2017-12-27 15:58:55 -0500

    Basic Functionality
    Just got the basic game movement working.

---
+++ Assets/Scripts/Launcher/NewUI/LauncherFlatUI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherFlatUI : MonoBehaviour {
+
+    public GameObject gameLabelContainer;
+    public NUI_GameLabel[] gameLabels;
+
+    public int gameSelector = 0;
+    public int playlistSelector = 0;
+
+	// Use this for initialization
+	void Start () {
+        gameLabels = gameLabelContainer.GetComponentsInChildren<NUI_GameLabel>();
+
+        RefreshUI();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            MoveUp();
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            MoveDown();
+	}
+
+    private void MoveUp()
+    {
+        //if(gameSelector > 0) 
+        gameSelector--;
+        
+        RefreshUI();
+    }
+
+    private void MoveDown()
+    {
+        //if(gameSelector < GM.data.playlists[playlistSelector].games.Count) 
+        gameSelector++;
+        
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        foreach (NUI_GameLabel label in gameLabels)
+        {
+            if(label.position + gameSelector >= 0 && label.position + gameSelector < GM.data.playlists[playlistSelector].games.Count)
+            {
+                label.text.text = GM.data.playlists[playlistSelector].games[gameSelector + label.position].name.ToUpper();
+            }
+
+            else
+            {
+                label.text.text = "";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Launcher/NewUI/NUI_GameLabel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NUI_GameLabel : MonoBehaviour {
+
+    public Text text;
+    public int position;
+    public GameObject targetAbove;
+    public GameObject target;
+    public GameObject targetBelow;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveUp()
+    {
+        SetToTarget(targetBelow);
+        TweenToTarget();
+    }
+
+    public void MoveDown()
+    {
+        SetToTarget(targetAbove);
+        TweenToTarget();
+    }
+
+    private void SetToTarget(GameObject obj)
+    {
+        transform.localPosition = obj.transform.localPosition;
+        transform.localRotation = obj.transform.localRotation;
+    }
+
+    private void TweenToTarget()
+    {
+        //Will be tweened but for now snap
+        transform.localPosition = target.transform.localPosition;
+        transform.localRotation = target.transform.localRotation;
+    }
+}
