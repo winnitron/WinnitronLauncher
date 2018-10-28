7db2fc6099695795a2801e5e81bbda8c2d196bdb
Marlon Wiebe <mr.m.wiebe@gmail.com>
2018-10-28 13:42:34 -0500

    Everything in the scene has been switched over to Text Mesh Pro.  Need to work on the code now.

--- Assets/Scripts/Launcher/LauncherGameLabel.cs
+++ Assets/Scripts/Launcher/LauncherGameLabel.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LauncherGameLabel : MonoBehaviour {
 
-    public Text text;
+    public TextMeshProUGUI text;
     public int position;
 
     public Tweenable tween;
